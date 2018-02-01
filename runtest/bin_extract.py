74aca1379069d4f8f5e8a8438b02a89ff017c424
ugurkaynar <ugurkaynar@ugurkaynars-MacBook-Pro.local>
2018-02-01 11:57:51 -0500

    updated bin parser

--- runtest/bin_extract.py
+++ runtest/bin_extract.py
@@ -12,21 +12,22 @@ import os
 import numpy as np
 
 
-if len(sys.argv) != 2:
-        print ("Usage: ./bin_extract <folder name>")
+if len(sys.argv) != 3:
+        print ("Usage: ./bin_extract <folder name> <bin_size>")
 	exit(0)
 
 
 lat2,dat,perc,binarray=[],[],[],[]
-binsize=25
-name=sys.argv[1]+str(binsize)+"bin."
+binsize=int(sys.argv[2])
+name=sys.argv[1].split(".")[0]
 
 fname =  open(sys.argv[1],"r")
-out_file = open (name+"latency", 'w')
-outRT=name+"rt.hist.csv"
+outRT=name+"-RT.csv"
 warmup=20000
 
+
 lenght = sum(1 for line in open(sys.argv[1]))
+lenght-=warmup
 for i in range(1000):
 	lat2.append(int(0))
 for line in fname:
@@ -34,30 +35,30 @@ for line in fname:
 	jobid=value[0].split("-")[2].split(".")[0].split("job")[1]
 	if(int(jobid)>=int(warmup)):
 		latency=float(value[3].split("\n")[0])
-		dat.append(latency)		
-
+		dat.append(latency*1000)		
 fname.close()
-
+#
 fname= open(outRT, "w")
-for i in range(0,len(dat),binsize):
-	binarray.append(float(i)/100)
+for i in range(binsize,len(dat),binsize):
+	binarray.append(float(i))
 
 bins = np.array(binarray)
 data = np.array(dat)
-inds = np.digitize(data, bins,right=True)
-
+inds = np.digitize(data, bins,right=False)
+#
 counter=collections.Counter(inds)
 count=0
 sum2=0
-for i in range(int(1000/(float(binsize)/100))):
+
+for i in range(int(1000)):
 	sum2 += counter[i]/float(lenght)
 	perc.append(sum2)
 
-
 for i in range(len(lat2)):
-        line= str(count)+"-"+str(count+float(binsize)/100) +","+str(int(counter[i])) +","+ str(perc[i])+"\n"
+
+        line= str(int(count))+"-"+str(int(count+float(binsize))) +","+str(int(counter[i])) +","+ str(perc[i])+"\n"
         fname.write(line)
-        count+=float(binsize)/100
+        count+=float(binsize)
 #	print str(count)+"-"+str(count+float(binsize)/100), str(int(counter[i])),str(perc[i])
 fname.close()
 
